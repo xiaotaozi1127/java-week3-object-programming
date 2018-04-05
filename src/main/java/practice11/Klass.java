13f4975a5e0033da49610f287c48ab93986d2051
htao <htao@thoughtworks.com>
2018-04-05 21:18:41 +0800

    [taohui] implement practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,39 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    List<Student> students = new ArrayList<>();
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student) {
+        if(students.contains(student)){
+            this.leader = student;
+        }
+        else{
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,46 @@
 package practice11;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private HashSet<Klass> klasses = new HashSet<>();
+
+    public Teacher(int id, String name, int age, HashSet<Klass> klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public HashSet<Klass> getClasses() {
+        return klasses;
+    }
+
+    @Override
+    public String introduce() {
+        String ownedClasses = klasses.stream()
+                .sorted((o1, o2) -> o1.getNumber() > o2.getNumber() ? 1 : -1)
+                .map(k -> new Integer(k.getNumber()).toString())
+                .collect(Collectors.joining(", "));
+        if(klasses.size() > 0){
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %s.", ownedClasses);
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public boolean isTeaching(Student student) {
+        return klasses.stream()
+                .filter(k -> k.getNumber() == student.getKlass().getNumber())
+                .collect(Collectors.toList()).size() > 0;
+    }
+
+    public String introduceWith(Student student) {
+        if(isTeaching(student)){
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+        }
+        return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
 }

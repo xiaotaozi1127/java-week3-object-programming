1153cc5554e74bf9365e6cb5ffbe6d2ad403a162
htao <htao@thoughtworks.com>
2018-04-05 20:56:58 +0800

    [taohui] finish practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,22 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(klass.getLeader() == this){
+            return super.introduce() + String.format(" I am a Student. I am Leader of %s.", klass.getDisplayName());
+        }
+        return super.introduce() + String.format(" I am a Student. I am at %s.", klass.getDisplayName());
+    }
 }
